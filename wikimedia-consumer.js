2fe12803651fe8795200794bfed0993816f1c0c8
Gayan <gayanhewa@gmail.com>
2022-08-28 18:31:41 +1000

    Update the consumer to use the opensearch node client so we can push the data that we consume from kafka topic to opensearch index wikimedia

--- wikimedia-consumer.js
+++ wikimedia-consumer.js
@@ -1,22 +1,36 @@
 const { Kafka } = require('kafkajs');
+const { Client } = require("@opensearch-project/opensearch");
+
+const client = new Client({
+  node: process.env.OS_HOST,
+  auth: {
+    username: process.env.OS_USERNAME,
+    password: process.env.OS_PASSWORD
+  },
+});
+
 const kafka = new Kafka({
   clientId: 'wikimedia-producer',
   brokers: ['localhost:9092'],
 });
 
-const consumer = kafka.consumer({ groupId: 'wikimedia-group' })
+const consumer = kafka.consumer({ groupId: 'wikimedia-group-v2' })
 
 const runConsumer = async () => {
-await consumer.connect()
-await consumer.subscribe({ topic: 'wikimedia-topics', fromBeginning: true })
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'wikimedia-topics', fromBeginning: true })
 
-await consumer.run({
-  eachMessage: async ({ topic, partition, message }) => {
-    console.log({
-      value: message.value.toString(),
-    })
-  },
-});
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            const {id} = JSON.parse(message.value.toString());
+            await client.index({
+                id: id,
+                index: 'wikimedia',
+                body: message.value.toString(),
+                refresh: true,
+            });
+        },
+    });
 };
 
 runConsumer()
